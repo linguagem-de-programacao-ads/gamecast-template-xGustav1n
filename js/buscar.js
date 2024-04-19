b6933ed830734e4a22cd2312b84c565ae0c6e7f1
ericarodri <erica.cunha@sptech.school>
2024-04-18 21:19:20 -0300

    Aula18/04

--- js/buscar.js
+++ js/buscar.js
@@ -10,8 +10,33 @@ async function buscar(){
     const cards = document.getElementById("cards_games")
 
     cards.innerHTML = respotaDadosAgenda.map((itemAgenda)=> {
-        return "TESTE "
-    });
+        return `
+        <div class="cardItem">
+        <div class="dataGame"> 
+            <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo} 
+        </div>
+        <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+        <div class="descricao">
+            <p> <strong>${itemAgenda.nome}</strong> </p>
+            <p>${itemAgenda.descricao}</p>
+            <p><strong>Gamers: </strong></p>
+
+            <div class="gamers">${  
+                    itemAgenda.gamers.map((gamerItem => {
+                        return `<div class="gamerItem">${gamerItem}</div>`
+                    })).join('')
+                }
+            </div>
+
+          
+        </div>  
+        <div class="assistir">
+                <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+        </div>
+    </div>
+    `
+    
+    }).join('');
 
 }
 
